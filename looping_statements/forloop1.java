09bec408e38059743bf1ae4c56b2bb4115b8fc0e
Mahadevprasad D L <185583240+Mahadevprasad-DL@users.noreply.github.com>
2025-01-01 23:55:32 +0530

    Add files via upload

---
+++ looping_statements/forloop1.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter your number:");
+        n = sc.nextInt();p
+        
+        for (int i = 1; i < 10; i++) { 
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+        
+        sc.close(); // Close the scanner 
+    }
+}
