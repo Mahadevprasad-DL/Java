228bb7c6c7b62d5d338a4406cffb3d6681f68282
Mahadevprasad D L <185583240+Mahadevprasad-DL@users.noreply.github.com>
2025-01-02 19:45:01 +0530

    Add files via upload

---
+++ looping_statements/dowhile.java
@@ -0,0 +1,15 @@
+package util;
+import java.util.Scanner;
+
+public class dowhile {
+	public static void main(String[] arg) {
+		int i=1, n=10;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=n);
+	}
+	
+
+}
